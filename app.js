e6ed0ca04fea3a1d55062678cf65850e42321daf
madeleinehuish <vasistha@hotmail.com>
2016-09-03 12:40:39 -0700

    saturday mid day

--- app.js
+++ app.js
@@ -1,63 +1,59 @@
-$('#button').click(function(){
-event.preventDefault();
-var dateValue = $('#dateEntered').val();
-var $xhr = $.getJSON('https://api.nasa.gov/planetary/apod?api_key=RC2ZEmLVWlfYOkOYT5MHXkOq82VWb85ejudwxtPm&date=' + dateValue +'');
+(function() {
+  'use strict';
 
-$xhr.done(function(data) {
-    if ($xhr.status !== 200) {
+  $('#button').click(function() {
+    event.preventDefault();
+    var dateValue = $('#dateEntered').val();
+    var $xhr = $.getJSON('https://api.nasa.gov/planetary/apod?api_key=RC2ZEmLVWlfYOkOYT5MHXkOq82VWb85ejudwxtPm&date=' + dateValue);
+
+    $xhr.done(function(data) {
+      if ($xhr.status !== 200) {
         return;
-    }
-    console.log(data);
-    console.log(data.hdurl);
+      }
 
-    var imageTitle = data.title;
-    var image = data.hdurl;
-    var description = data.explanation;
-    $('#titleGoesHere').html('<p>"' + imageTitle + '"</p>');
-    $('#photoGoesHere').html('<img src="' + image + '" alt="Space!" height="400" >');
-    $('#descriptionGoesHere').html('<p>' + description + '"</p>');
+      var imageTitle = data.title;
+      var image = data.hdurl;
+      var description = data.explanation;
+      var imageEl = '<img src="' + image + '" alt="Space!" height="400" >';
 
+      $('#titleGoesHere').html('<p class="desc">"' + imageTitle + '"</p>');
+      $('#photoGoesHere').html(imageEl);
+      $('#descriptionGoesHere').html('<p class="desc">' + description + '"</p>');
 
-$xhr.fail(function(err) {
-console.log(err);
-});
+      $xhr.fail(function(err) {
+        return err;
+      });
+    });
+  });
 
+  $('#marsButton').click(function() {
+    event.preventDefault();
+    var dateValue2 = 'sol=' + $('#dateEntered2').val() + '&';
+    var pageValue = 'page=' + $('#pageEntered').val();
+    var baseUrl = 'https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?';
+    var apiKey = 'api_key=RC2ZEmLVWlfYOkOYT5MHXkOq82VWb85ejudwxtPm&';
 
-})
-})
+    var $xhr = $.getJSON(baseUrl + dateValue2 + apiKey + pageValue);
 
-$('#marsButton').click(function(){
-event.preventDefault();
-var dateValue = $('#dateEntered').val();
-var $xhr = $.getJSON('https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1001&api_key=RC2ZEmLVWlfYOkOYT5MHXkOq82VWb85ejudwxtPm');
-
-$xhr.done(function(data2) {
-    if ($xhr.status !== 200) {
+    $xhr.done(function(data2) {
+      if ($xhr.status !== 200) {
         return;
-    }
-    console.log(data2);
-    // console.log(data2.photos[0].img_src);
+      }
 
-    // var imageTitle2 = data2.photos[0].camera.full_name;
-    // var image2 = data2.photos.img_src;
-    // var description = data.explanation;
-    // $('#title2GoesHere').html('<p>"Opportunity - ' + imageTitle2 + '"</p>');
-    var image2=[];
-    var imageTitle2=[];
-    for (var i = 0; i < 20; i++){
-        // var image2 = data2.photos[i].img_src;
+      for (var i = 0; i < 25; i++) {
         console.log(data2.photos[i].img_src);
         $('#title2GoesHere').append('<p>"Opportunity - ' + data2.photos[i].camera.full_name + '"</p><img src="' + data2.photos[i].img_src + '" alt="Space!" height="400" >');
-    }
+      }
 
-    // $('#description2GoesHere').html('<p>' + description + '"</p>');
+      $xhr.fail(function(err) {
+        return err;
+      });
+    });
+  });
 
+  $('#reload').click(function() {
+    location.reload();
+  });
 
-$xhr.fail(function(err) {
-console.log(err);
-});
-
-
-})
-})
- $(".button-collapse").sideNav();
+  $('.button-collapse').sideNav();
+})();
