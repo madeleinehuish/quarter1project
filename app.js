aa54b893f913ff966e89f137cade96e502ffbc37
madeleinehuish <vasistha@hotmail.com>
2016-09-04 04:50:26 -0700

    sunday very early morn

--- app.js
+++ app.js
@@ -2,7 +2,9 @@
   'use strict';
 
   $('#button').click(function() {
+
     event.preventDefault();
+
     var dateValue = $('#dateEntered').val();
     var $xhr = $.getJSON('https://api.nasa.gov/planetary/apod?api_key=RC2ZEmLVWlfYOkOYT5MHXkOq82VWb85ejudwxtPm&date=' + dateValue);
 
@@ -14,11 +16,11 @@
       var imageTitle = data.title;
       var image = data.hdurl;
       var description = data.explanation;
-      var imageEl = '<img src="' + image + '" alt="Space!" height="400" >';
+      var imageEl = '<img src="' + image + '" alt="Space!" height="400">';
 
       $('#titleGoesHere').html('<p class="desc">"' + imageTitle + '"</p>');
       $('#photoGoesHere').html(imageEl);
-      $('#descriptionGoesHere').html('<p class="desc">' + description + '"</p>');
+      $('#descriptionGoesHere').html('<p class="desc readableText">' + description + '"</p>');
 
       $xhr.fail(function(err) {
         return err;
@@ -27,11 +29,15 @@
   });
 
   $('#marsButton').click(function() {
+
     event.preventDefault();
+    $('#marsInstructions').hide();
+
+    var rover = $('input[name="rover"]:checked').val();
+    var baseUrl = 'https://api.nasa.gov/mars-photos/api/v1/rovers/' + rover + '/photos?';
     var dateValue2 = 'sol=' + $('#dateEntered2').val() + '&';
-    var pageValue = 'page=' + $('#pageEntered').val();
-    var baseUrl = 'https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?';
     var apiKey = 'api_key=RC2ZEmLVWlfYOkOYT5MHXkOq82VWb85ejudwxtPm&';
+    var pageValue = 'page=' + $('#pageEntered').val();
 
     var $xhr = $.getJSON(baseUrl + dateValue2 + apiKey + pageValue);
 
@@ -40,9 +46,9 @@
         return;
       }
 
+      $('#title2GoesHere').append('<br />');
       for (var i = 0; i < 25; i++) {
-        console.log(data2.photos[i].img_src);
-        $('#title2GoesHere').append('<p>"Opportunity - ' + data2.photos[i].camera.full_name + '"</p><img src="' + data2.photos[i].img_src + '" alt="Space!" height="400" >');
+        $('#title2GoesHere').append('<br /><br /><p>"'+ data2.photos[i].rover.name + ' ' + data2.photos[i].camera.full_name + ' ' + data2.photos[i].earth_date +'"</p><img src="' + data2.photos[i].img_src + '" alt="Space!" height="400" >');
       }
 
       $xhr.fail(function(err) {
