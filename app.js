53d131808a72b19e78dd4d7081721912a4831db9
madeleinehuish <vasistha@hotmail.com>
2016-09-04 06:19:49 -0700

    sunday early morn, added wikipedia

--- app.js
+++ app.js
@@ -28,6 +28,34 @@
     });
   });
 
+  $('#marsBackgroundButton').click(function(){
+
+    event.preventDefault();
+    $('#marsInstructions').hide();
+
+    var rover = $('input[name="rover"]:checked').val();
+    rover = toTitleCase(rover);
+
+
+      $.ajax({
+         type: "GET",
+         url: "http://en.wikipedia.org/w/api.php?action=parse&format=json&prop=text&page=" + rover +"_(rover)&callback=?",
+        //  contentType: "application/json; charset=utf-8",
+        //  async: false,
+         dataType: "json",
+         success: function (data, textStatus, jqXHR) {
+
+             var markup = data.parse.text["*"];
+             var blurb = $('<div></div>').html(markup);
+             $('#backgroundGoesHere').html($(blurb).find('p'));
+
+         },
+         error: function (errorMessage) {
+         }
+      });
+  })
+
+
   $('#marsButton').click(function() {
 
     event.preventDefault();
@@ -60,6 +88,10 @@
   $('#reload').click(function() {
     location.reload();
   });
+
+  function toTitleCase(str){
+    return str.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
+  }
 
   $('.button-collapse').sideNav();
 })();
